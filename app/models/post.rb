29ce9d5fd9349ab9df6efa7e42fea590a643b615
Vinoth <vinothkumarfrta@gmail.com>
2015-01-06 13:08:00 +0530

    used pg_search gem for search

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ActiveRecord::Base
+  include PgSearch
+
+  pg_search_scope :search, against: [:name]
+
   def self.text_search(query)
     if query.present?
-      where("name ilike :q", q: "%#{query}%")
+      search(query)
     else
       all
     end
